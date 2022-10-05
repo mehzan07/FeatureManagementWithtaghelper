7b0d6e025663becbc09a73337c6590344f96797a
Mehrdad <mehzan07@yahoo.com>
2022-10-05 19:20:15 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FeatureManagement.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IFeatureManager _featureManager;
+
+        public IndexModel(ILogger<IndexModel> logger, IFeatureManager featureManager)
+        {
+            _logger = logger;
+            _featureManager = featureManager;
+        }
+
+        public async void OnGet()
+        {
+            _logger.LogInformation($"feature value {await _featureManager.IsEnabledAsync(FeatureFlags.SecretFeature)}");
+        }
+    }
+
+    public static class FeatureFlags
+    {
+        public static readonly string SecretFeature = nameof(SecretFeature);
+    }
+}
